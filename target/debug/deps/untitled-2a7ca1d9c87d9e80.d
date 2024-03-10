d603362fb7424f79d8a8858b35a9c6a9feda3162
HajiAli <senayousofiali@gmail.com>
2024-03-10 21:09:56 +0430

    variables

---
+++ target/debug/deps/untitled-2a7ca1d9c87d9e80.d
@@ -0,0 +1,5 @@
+C:\Users\DELL\RustroverProjects\rust_at_last\target\debug\deps\untitled-2a7ca1d9c87d9e80.exe: src\main.rs
+
+C:\Users\DELL\RustroverProjects\rust_at_last\target\debug\deps\untitled-2a7ca1d9c87d9e80.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/untitled.d
@@ -0,0 +1,1 @@
+C:\Users\DELL\RustroverProjects\rust_at_last\target\debug\untitled.exe: C:\Users\DELL\RustroverProjects\rust_at_last\src\main.rs
